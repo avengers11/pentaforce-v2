5aa7c161aee123d680ff527d1eadaf22e73c3866
masudranadeveloper <masud1010rana0101@gmail.com>
2023-12-31 11:02:24 +0600

    first commit

---
+++ storage/framework/views/2ece06e9710c8cb2f3229395945654ce18a0d1e6.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Server Error')); ?>
+<?php $__env->startSection('code', '500'); ?>
+<?php $__env->startSection('message', __('Server Error')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\Laravel\codecanyon_EuK0LLc4_openai_davinci_ai_writing_assistant_and_content\openaidavinci-3.5\resources\views/errors/500.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/3d6c02b3b282e594f64c48600ce21afbf1cf4ad8.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Page Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Ooops! Looks like it is not here')); ?>
+
+<?php echo $__env->make('errors::layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\Laravel\codecanyon_EuK0LLc4_openai_davinci_ai_writing_assistant_and_content\openaidavinci-3.5\resources\views/errors/404.blade.php ENDPATH**/ ?>
